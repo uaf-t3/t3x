654928f80b666201799d5b73fad5b693d4aa768b
SushiFanta <sushifanta@DESKTOP-1RQBRAS>
2024-03-05 15:20:54 -0900

    remove sudo inside script because that's bad practice

--- scripts/sshdisable.sh
+++ scripts/sshdisable.sh
@@ -2,7 +2,7 @@
 t3ize_sh="${0%/*}/../lib/t3ize.sh"
 source "$t3ize_sh"
 
-#stop ssh and disable s
+#stop ssh and disable ssh on startup. Requires root privelege
 echo "attempting to disable ssh"
-sudo systemctl stop ssh.service
-sudo systemctl disable ssh.service
+systemctl stop ssh.service
+systemctl disable ssh.service
