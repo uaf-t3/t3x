dabdafe3e29fa1e978d08ca7f4243e3660954d62
Dayne Broderson <broderson@gmail.com>
2024-03-06 19:30:43 -0900

    dbb tweak to vncdisable

--- scripts/vncdisable.sh
+++ scripts/vncdisable.sh
@@ -4,7 +4,8 @@ source "$t3ize_sh"
 
 # Check if vncserver command exists
 if !(command -v vncserver &> /dev/null); then
-  boom "VNC server was not found to be installed."
+   warn "VNC server not detected ... exiting"
+   exit 0
 fi
 
 #stop ssh and disable ssh on startup. Requires root privelege
@@ -12,16 +13,16 @@ yak "Attempting to disable all VNC servers using sudo to systemctl"
 run "sudo systemctl stop vncserver@*"
 if [ $? -eq 0 ]; then
   yak "Successful stop of running VNC server(s)"
-  SSH_STOP=true
+  VNC_STOP=true
 else
-  boom "Unable to stop VNC server(s)" # check if this is valid boom or just info
+  boom "Unable to stop VNC server(s)" 
 fi
 run "sudo systemctl disable vncserver@*"
 if [ $? -eq 0 ]; then
   yak "Successful disabling of VNC server(s) on start"
-  SSH_DISABLE=true
+  VNC_DISABLE=true
 else
-  boom "Unable to disbale VNC" 
+  boom "Unable to disable VNC" 
 fi
 
 if [ "$SSH_STOP" = "true" ] && [ "$SSH_DISABLE" = "true" ]; then
@@ -29,4 +30,7 @@ if [ "$SSH_STOP" = "true" ] && [ "$SSH_DISABLE" = "true" ]; then
   sleep 1
 else
   warn "VNC not fully disabled"
+  exit 1
 fi
+
+exit 0
