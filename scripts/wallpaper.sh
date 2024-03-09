720529c56931f88679aeff7b672d203c8b323abc
Petie Deveer <50847801+ItalianSquirel@users.noreply.github.com>
2024-03-08 22:36:57 -0900

    Merge P1 to main branch (#11)
    * Made wallpaper.sh to change wallpaper via URL
    * p1d consideration for p1
    * LICENSE, CHANGELOG, and README
    * fix: README MD format
    * :scroll: :mute:
    * T3X
    * cleanup LICENSE
    * LICE tweak
    * create a script to stop and disable ssh
    * remove sudo inside script because that's bad practice
    * actually sudo keyword is good. Fixed exec permissions for ssh script.
    * mega hack
    * remove d.pico things
    * a poem
    * use run() and typo fix
    * warning->warning to sync with main
    
    ---------
    
    Co-authored-by: Dayne Broderson <broderson@gmail.com>
    Co-authored-by: SushiFanta <sushifanta@DESKTOP-1RQBRAS>

---
+++ scripts/wallpaper.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+t3ize_sh="${0%/*}/../lib/t3ize.sh"
+source "$t3ize_sh"
+
+# Change Wallpaper
+IMAGE_URL="https://raw.githubusercontent.com/ItalianSquirel/T3-RPi-Image/main/Assets/Wallpapers/t3wallpaper.png"
+
+if [ -d $HOME/Pictures ]; then
+  WALLPAPER_DIR=$HOME/Pictures
+else
+  WALLPAPER_DIR="$HOME/.local/share/rpx/"
+fi
+
+WALLPAPER_FILE="$WALLPAPER_DIR/$(basename $IMAGE_URL)"
+
+info "Downloading T3 Wallpaper"
+if [ ! -d $WALLPAPER_DIR ]; then
+  run "mkdir -p $WALLPAPER_DIR"
+fi
+
+# Download the image file to the Raspberry Pi
+require_command wget
+run "wget -q -O ${WALLPAPER_FILE} ${IMAGE_URL}"
+if [ $? -eq 0 ]; then
+  yak "T3X wallpaper download success"
+else
+  boom "Download of wallpaper failed"
+fi
+
+if got_command pcmanfm; then
+  yak "pcmanfm command found - setting the image file as the wallpaper"
+  run "pcmanfm --set-wallpaper=${WALLPAPER_FILE}"
+  yak "Refresh the desktop to show the new wallpaper"
+  run "pcmanfm -w /"
+  cowsay "Look at that" | lolcat
+else
+  warn "pcmanfm command not found - skipping applying of wallpaper"
+  cowsay "Manual wallpaper install time"
+  warn "New wallpaper location: $WALLPAPER_FILE"
+fi
+
+exit 0
