6407b065df5d47dcc8cab3108101b9dff286ae09
ItalianSquirel <petiedeveer@gmail.com>
2024-03-18 12:55:00 -0800

    Fixed sanity.sh to use "sudo apt install" instead of apt_install. (Wasn't Working)

--- scripts/sanity.sh
+++ scripts/sanity.sh
@@ -4,9 +4,9 @@ t3ize_sh="${0%/*}/../lib/t3ize.sh"
 source "$t3ize_sh"
 
 yak "sanity check initiated"
-apt_install "figlet toilet cowsay fortune lolcat"
-apt_install "jq curl git vim tmux htop"
-apt_install "pv"
+sudo apt install "figlet toilet cowsay fortune lolcat"
+sudo apt install "jq curl git vim tmux htop"
+sudo apt install "pv"
 figlet "T3X" | lolcat
 
 echo -n "sanity check success"; sleep 1; echo " . . . we hope" | pv -qL 5 | lolcat
