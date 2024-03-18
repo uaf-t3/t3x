e4301834af4a3aca514c09a3124d6cdbb0d5c9d7
ItalianSquirel <petiedeveer@gmail.com>
2024-03-18 15:29:14 -0800

    fixed wierd update comand i put in

--- scripts/node_installer.sh
+++ scripts/node_installer.sh
@@ -3,9 +3,6 @@
 # File containing the list of packages to install
 NODE_LIST="${0%/*}/src/nodes.txt"
 
-# Update package list to ensure packages are from the latest repository
-apt update
-
 while IFS= read -r node
 do
     # Install each package listed in the packages.txt file
