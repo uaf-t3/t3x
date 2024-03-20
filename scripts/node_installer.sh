ba438e13259d56b68c89eaf8402ba9c5a95a2350
ItalianSquirel <petiedeveer@gmail.com>
2024-03-19 20:40:03 -0800

    node_installer.sh installs with NPM

--- scripts/node_installer.sh
+++ scripts/node_installer.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# File containing the list of packages to install
+# File containing the list of npm packages to install
 NODE_LIST="${0%/*}/src/nodes.txt"
 
+# Loop through the nodes.txt file
 while IFS= read -r node
 do
-    # Install each package listed in the packages.txt file
-    sudo apt install -y "$node"
+    # Install each npm package listed in the nodes.txt file
+    # This installs packages globally; remove -g flag if local installation is desired
+    npm install -g "$node"
 done < "$NODE_LIST"
