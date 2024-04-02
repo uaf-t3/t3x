159625f60c5d21235c01f6943731d1fe7c5b3e0d
Dayne Broderson <broderson@gmail.com>
2024-04-01 23:33:34 -0800

    check for t3x/bin dir

--- skel/t3x.bash.d/00-t3x.sh
+++ skel/t3x.bash.d/00-t3x.sh
@@ -1,6 +1,6 @@
 # add t3x to $PATH
 echo "t3x enabled"
-if [ -f $HOME/t3x/bin ]; then
+if [ -d $HOME/t3x/bin ]; then
   if ! $(echo $PATH | grep "t3x/bin" >/dev/null); then
     export PATH=$PATH:$HOME/t3x/bin;
   fi
