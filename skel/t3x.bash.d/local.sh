02232a82e86c78e3746c31b2b53abc08400a8106
Dayne Broderson <broderson@gmail.com>
2024-04-01 23:34:54 -0800

    again, dir check

--- skel/t3x.bash.d/local.sh
+++ skel/t3x.bash.d/local.sh
@@ -1,5 +1,5 @@
 # add $HOME/.local/bin to PATH
-if [ -f $HOME/.local/bin ]; then
+if [ -d $HOME/.local/bin ]; then
   if ! $(echo $PATH | grep ".local/bin" > /dev/null); then
     export PATH=$PATH:$HOME/.local/bin
   fi
