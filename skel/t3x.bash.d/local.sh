7cb865ce0ef7e9273ac1ddc6465218b6c698f4b2
Dayne Broderson <broderson@gmail.com>
2024-04-01 23:37:13 -0800

    T3X comment on local.sh

--- skel/t3x.bash.d/local.sh
+++ skel/t3x.bash.d/local.sh
@@ -1,4 +1,4 @@
-# add $HOME/.local/bin to PATH
+# T3x # Add $HOME/.local/bin to PATH
 if [ -d $HOME/.local/bin ]; then
   if ! $(echo $PATH | grep ".local/bin" > /dev/null); then
     export PATH=$PATH:$HOME/.local/bin
