722d17cfa4f95525e1d1c3eab5500f9615545dc3
Dayne Broderson <dayne@omg.lol>
2024-04-10 14:53:35 -0800

    remove nerdfont code from starship script

--- tools/starship/setup-starship.sh
+++ tools/starship/setup-starship.sh
@@ -33,44 +33,4 @@ function starship_check() {
   fi
 }
 
-# TODO: nerdfont
-function nerdfont_install() {
-  #local FONTZIP_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip"
-  #FONTZIP_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Ubuntu.zip"
-  FONTZIP_URL_DEFAULT="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/UbuntuMono.zip"
-  FONTZIP_URL=${1:-$FONTZIP_URL_DEFAULT}
-  local ZIPFILE=$(basename $FONTZIP_URL)
-  echo "starting nerdfont install: $ZIPFILE"
-  local ZIPCACHE="$HOME/.local/share/fonts/zipcache"
-  if [ ! -d "$ZIPCACHE" ]; then
-    echo "making ZIPCACHE: $ZIPCACHE"
-    mkdir -p "$ZIPCACHE"
-  fi
-
-  if ! command -v wget > /dev/null 2>&1 ; then
-    echo "missing wget - unable to download"
-    echo "fix: sudo apt install wget"
-    exit 1
-  fi
-  pushd . > /dev/null
-
-  cd $ZIPCACHE
-  if [ ! -f "$ZIPFILE" ]; then
-    echo "downloading $FONTZIP_URL"
-    wget --quiet "$FONTZIP_URL" || boom "failed: wget $FONTZIP_URL"
-  else
-    echo "skipping download (file exists): $FONTZIP_URL"
-  fi 
-  cd ..
-  echo "unzipping $ZIPFILE in `pwd`"
-  unzip -n -qq zipcache/$ZIPFILE > /dev/null  
-  if [ ! $? -eq 0 ]; then
-    boom "failed: unzip zipcache/$ZIPFILE"
-  fi
-  echo "updating font cache: fc-cache -fv"
-  fc-cache -fv > /dev/null 2>&1 || boom "failed: fc-fache -fv"
-  popd
-}
-
 starship_check
-nerdfont_install
