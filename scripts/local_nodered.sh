ebcf9a8c56efc68bc95e491df0a7fa438158863c
SushiFanta <sushifanta@DESKTOP-1RQBRAS>
2024-05-20 15:14:51 -0800

    move functions above their calls and resolve syntax issue in awk call

--- scripts/local_nodered.sh
+++ scripts/local_nodered.sh
@@ -2,8 +2,6 @@
 
 # Define the path to the Node-RED settings file
 SETTINGS_FILE="$HOME/.node-red/settings.js"
-echo "calling installation function..."
-install_nodered
 
 #apply the default settings file and install node red. This should be locked down by default.
 function install_nodered
@@ -24,7 +22,7 @@ function lockdown_nodered
     # Check if 'uiHost' is already configured
     if grep -q "uiHost" "$SETTINGS_FILE"; then
         # replace all lines with "uihost" so that they target the loopback interface
-        awk '/uiHost:/ {gsub "uiHost: \'127.0.0.1\',"); print} 1' "$SETTINGS_FILE.bak" > "$SETTINGS_FILE"
+        awk '/uiHost:/ {gsub "uiHost: \'127.0.0.1\\',"); print} 1' "$SETTINGS_FILE.bak" > "$SETTINGS_FILE"
         echo "Updated 'uiHost' to '127.0.0.1' in the settings file."
     else
         # 'uiHost' not found; add it to the settings file
@@ -38,3 +36,7 @@ function lockdown_nodered
 
     echo "Node-RED configuration updated. Please verify that Node-RED is running locally as intended."
 }
+
+#install node red
+echo "calling installation function..."
+install_nodered
