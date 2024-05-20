2eef0b2247d2463609628096c5f05e693dc35cce
SushiFanta <sushifanta@DESKTOP-1RQBRAS>
2024-05-20 15:07:01 -0800

    create default settings file and update script to load it as well as add lockdown feature

--- scripts/local_nodered.sh
+++ scripts/local_nodered.sh
@@ -1,29 +1,40 @@
 #!/bin/bash
 
-#install or update node red
-#skips confirmations
-#commented out for debugging
-#bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-root --confirm-install --confirm-pi --no-init
-
 # Define the path to the Node-RED settings file
 SETTINGS_FILE="$HOME/.node-red/settings.js"
+echo "calling installation function..."
+install
 
-# Backup the original settings file
-cp "$SETTINGS_FILE" "$SETTINGS_FILE.bak"
+#apply the default settings file and install node red. This should be locked down by default.
+function install
+{
+    echo "restoring node red settings from default.."
+    cp "../lib/node-red-default-settings.js" "$SETTINGS_FILE"
 
-# Check if 'uiHost' is already configured
-if grep -q "uiHost" "$SETTINGS_FILE"; then
-    # 'uiHost' is found; ensure it's set to '127.0.0.1'
-    awk '/uiHost/ && !/127.0.0.1/ {sub(/uiHost:.*/, "uiHost: \'127.0.0.1\',")} 1' "$SETTINGS_FILE.bak" > "$SETTINGS_FILE"
-    echo "Updated 'uiHost' to '127.0.0.1' in the settings file."
-else
-    # 'uiHost' not found; add it to the settings file
-    echo "uiHost: '127.0.0.1'," >> "$SETTINGS_FILE"
-    echo "Added 'uiHost' setting to run locally."
-fi
+    echo "running node red installer..."
+    echo bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-root --confirm-install --confirm-pi --no-init
+}
 
-# Restart Node-RED to apply changes
-echo "Attempting to restart Node-RED..."
-node-red-restart
+# modify the settings to bind node red to be hosted locally, then restart node red to apply the changes.
+function lockdown
+{
+    # Backup the original settings file
+    cp "$SETTINGS_FILE" "$SETTINGS_FILE.bak"
 
-echo "Node-RED configuration updated. Please verify that Node-RED is running locally as intended."
+    # Check if 'uiHost' is already configured
+    if grep -q "uiHost" "$SETTINGS_FILE"; then
+        # replace all lines with "uihost" so that they target the loopback interface
+        awk '/uiHost:/ {gsub "uiHost: \'127.0.0.1\',"); print} 1' "$SETTINGS_FILE.bak" > "$SETTINGS_FILE"
+        echo "Updated 'uiHost' to '127.0.0.1' in the settings file."
+    else
+        # 'uiHost' not found; add it to the settings file
+        echo "uiHost: '127.0.0.1'," >> "$SETTINGS_FILE"
+        echo "Added 'uiHost' setting to run locally."
+    fi
+
+    # Restart Node-RED to apply changes
+    echo "Attempting to restart Node-RED..."
+    node-red-restart
+
+    echo "Node-RED configuration updated. Please verify that Node-RED is running locally as intended."
+}
