7a77aeca4eb0d0da51d78a3628c9167b845019a8
SushiFanta <sushifanta@DESKTOP-1RQBRAS>
2024-05-21 12:48:33 -0800

    configure template for pw auth and begin migration to nodered tool script

--- scripts/local_nodered.sh
+++ scripts/local_nodered.sh
@@ -6,7 +6,15 @@ SETTINGS_FILE="$HOME/.node-red/settings.js"
 #defind the path to the default settings file
 DEFAULT_FILE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../lib/node-red-default-settings.js"
 
-#apply the default settings file and install node red. This should be locked down by default.
+# Restart Node-RED to apply changes
+function restart_nodered
+{
+    echo "Attempting to restart Node-RED..."
+    node-red-restart
+    echo "Node-RED configuration updated. Please verify that Node-RED is running locally as intended."
+}
+
+#apply the default settings file and install node red.
 function install_nodered
 {
     echo "running node red installer..."
@@ -21,9 +29,6 @@ function install_nodered
     else
         echo "successfully loaded default settings."
     fi
-
-    echo "locking down node red by binding it to be hosted locally..."
-    lockdown_nodered
 }
 
 # modify the settings to bind node red to be hosted locally, then restart node red to apply the changes.
@@ -37,7 +42,7 @@ function lockdown_nodered
         # replace all lines with "uihost" so that they target the loopback interface
         awk '{if ($0 ~ /uiHost:/) print "\tuiHost: \"127.0.0.1\","; else print}' "$SETTINGS_FILE.bak" > "$SETTINGS_FILE"
         if [ $? -ne 0 ]; then
-            echo "Failed to bind node red to be hosted locally. Avoid running node red without setting a password."
+            echo "Failed to bind node red to be hosted locally. Do not run node red unless remote authentication is enabled."
         exit 1
         else
             echo "Updated 'uiHost' to '127.0.0.1' in the settings file."
@@ -47,14 +52,40 @@ function lockdown_nodered
         echo "uiHost: '127.0.0.1'," >> "$SETTINGS_FILE"
         echo "Added 'uiHost' setting to run locally."
     fi
-
-    # Restart Node-RED to apply changes
-    echo "Attempting to restart Node-RED..."
-    node-red-restart
-
-    echo "Node-RED configuration updated. Please verify that Node-RED is running locally as intended."
 }
 
-#install node red
-echo "calling installation function..."
+# modify the settings to enable password authentication, then restart node red to apply the changes.
+function enable_remote_nodered
+{
+    # Backup the original settings file
+    cp "$SETTINGS_FILE" "$SETTINGS_FILE.bak"
+
+    # Check if 'uiHost' is already configured
+    if grep -q "uiHost:" "$SETTINGS_FILE"; then
+        # replace all lines with "uihost" so that they target the loopback interface
+        awk '{if ($0 ~ /uiHost:/) print "\tuiHost: \"127.0.0.1\","; else print}' "$SETTINGS_FILE.bak" > "$SETTINGS_FILE"
+        if [ $? -ne 0 ]; then
+            echo "Failed to bind node red to be hosted locally. Do not run node red unless remote access feature is enabled intentionally."
+        exit 1
+        else
+            echo "Updated 'uiHost' to '127.0.0.1' in the settings file."
+        fi
+    else
+        # 'uiHost' not found; add it to the settings file
+        echo "uiHost: '127.0.0.1'," >> "$SETTINGS_FILE"
+        echo "Added 'uiHost' setting to run locally."
+    fi
+}
+
+#install and lockdown node red
+echo "running node red installer..."
 install_nodered
+echo "locking down node red by binding it to be hosted locally..."
+lockdown_nodered
+if [ $? -ne 0 ]; then
+    echo "Failed to bind node red to be hosted locally. Avoid running node red without setting a password."
+    exit 1
+else
+    echo "restarting node red to apply the changes..."
+    restart_nodered
+fi
