eed4ac2cd839f115bd21b5ff2c0ac7b3991bf7b7
SushiFanta <sushifanta@DESKTOP-1RQBRAS>
2024-06-03 10:57:11 -0800

    cached copy of installer

--- tools/nodered/scripts/setup.sh
+++ tools/nodered/scripts/setup.sh
@@ -4,20 +4,33 @@ source $(t3x -E)
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null; pwd)
 # Define the path to the template settings file for Node-RED
 SETTINGS_TEMPLATE="$SCRIPT_DIR/../files/node-red-default-settings.js"
+#define the path to cache the installer file
+CACHED_INSTALLER="$SCRIPT_DIR/../files/update-nodejs-and-nodered"
+INSTALLER_URL="https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered"
 # Define the path to the Node-RED settings file
 SETTINGS_FILE="$HOME/.node-red/settings.js"
 
-echo "running node red installer..."
-bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-root --confirm-install --confirm-pi --no-init
+echo "getting latest Node-RED installer..."
+if curl -sL "$INSTALLER_URL" -o "$CACHED_INSTALLER"; then
+    echo "Installer downloaded successfully."
+else
+    echo "Failed to download the installer. Attempting to run cached version..."
+    exit 1
+fi
+if [ -f "$CACHED_INSTALLER" ]; then
+    echo "running installer..."
+    bash "$CACHED_INSTALLER" --confirm-root --confirm-install --confirm-pi --no-init
+else
+    echo "Cached Node-RED installer not found. Are you connected to the internet?."
+    exit 1
+fi
 
 echo "restoring Node-RED settings from t3x default..."
-cp "$SETTINGS_TEMPLATE" "$SETTINGS_FILE"
-# Check if the cp command succeeded
-if [ $? -ne 0 ]; then
+if cp "$SETTINGS_TEMPLATE" "$SETTINGS_FILE"; then
+    echo "successfully loaded default settings."
+else
     echo "Failed to copy from default settings file. Node-RED may not be locked down. Use the lockdown command to secure Node-RED."
     exit 1
-else
-    echo "successfully loaded default settings."
 fi
 
 echo "running installer for nodes..."
