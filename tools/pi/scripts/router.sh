7d010867c266ce9e31c79221f1f048cef601e940
LisaJacklin <lmjacklin@alaska.edu>
2024-08-02 17:24:09 -0800

    router skeleton

--- tools/pi/scripts/router.sh
+++ tools/pi/scripts/router.sh
@@ -1,4 +1,26 @@
 !/usr/bin/bash
 # source $(t3x -T)
 
+#think this may be needed..
+router_ip = "10.20.24.1"
 
+
+disable_route() {}
+
+enable_route() {}
+
+case "$1" in
+	enable)
+		enable_route
+		;;
+	disable)
+		disable_route
+		;;
+	*)
+		exit 1
+		;;
+esac
+
+#TODO
+# - adjust nft tables for enable/disable router..?
+# - check for table changes
