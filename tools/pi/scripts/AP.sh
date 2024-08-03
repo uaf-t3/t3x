31ca720a7b74a62e2b478bdfd71be63cf20c160c
LisaJacklin <lmjacklin@alaska.edu>
2024-08-02 17:00:08 -0800

    skeleton AP

--- tools/pi/scripts/AP.sh
+++ tools/pi/scripts/AP.sh
@@ -1,4 +1,23 @@
 !/usr/bin/bash
 #source $(t3x -T)
 
+#var set to AP SSID
+AP="RaspAP"
 
+AP_up() {}
+
+AP_down() {}
+
+
+case "$1" in
+	up)
+		# pull AP up
+		;;
+	down)
+		# pull AP down
+		;;
+	*)
+		# display command usage?
+		exit 1
+		;;
+esac
