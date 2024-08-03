0c7df379723740b687b7f39313cb2b17cdac784a
LisaJacklin <lmjacklin@alaska.edu>
2024-08-02 17:12:01 -0800

    skeleton status built

--- tools/pi/scripts/status.sh
+++ tools/pi/scripts/status.sh
@@ -1,4 +1,26 @@
 !/usr/bin/bash
 #source $(t3x -T)
 
+status_Router() {}
 
+status_AP() {}
+
+#main
+case "$1" in 
+	router)
+		status_Router
+		;;
+	ap)
+		status_AP
+		;;
+	*)
+		#usage?
+		;;
+esac
+
+#TODO
+# - fill the functions
+# - compare on and off for each function
+# - print usage?
+# - test on new pi
+# - test t3x compatability..
