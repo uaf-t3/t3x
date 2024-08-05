964ea96c6730ba0b36b3a56814157e4ac873a9ba
LisaJacklin <lmjacklin@alaska.edu>
2024-08-05 09:03:18 -0800

    connection checking for AP

--- tools/pi/scripts/AP.sh
+++ tools/pi/scripts/AP.sh
@@ -1,8 +1,11 @@
-!/usr/bin/bash
-#source $(t3x -T)
+#!/usr/bin/bash
+source $(t3x -T)
 
 #var set to AP SSID
 AP="RaspAP"
+
+connection_status=$(nmcli -t -f NAME connection show --active | grep "^$AP")
+
 
 AP_up() {
 	nmcli connection up "$AP"
@@ -12,9 +15,30 @@ AP_up() {
 AP_down() {
 	nmcli connection down "$AP"
 	echo "Access Point '$AP' has been turned off"
-
 }
 
+#check if AP has ever been connected to this dev
+first_con() {
+	nmcli dev wifi connection "$AP" password "raspberry"
+
+	if [$? -eq 0]; then
+		echo "Connection to $AP success"
+	else 
+		echo "Failed to connect to $AP"
+	fi
+}
+
+connection_check() {
+	if [ -f "/etc/wifi_connection" ]; then
+		if grep -q "$AP" "/etc/wifi-connection"; then
+			exit 0
+		else
+			first_con
+		fi
+	fi
+}
+
+connection_check
 
 case "$1" in
 	up)
