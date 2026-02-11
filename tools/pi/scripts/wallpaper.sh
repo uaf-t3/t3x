d8ebcd75fb599acc0e57a3046176079231ef2030
Dayne Broderson <dayne@omg.lol>
2026-02-10 21:48:52 -0900

    wallpaper run to new style

--- tools/pi/scripts/wallpaper.sh
+++ tools/pi/scripts/wallpaper.sh
@@ -16,10 +16,10 @@ WALLPAPER_FILE="$WALLPAPER_DIR/$(basename $IMAGE_URL)"
 require_command wget
 info "Downloading T3 Wallpaper"
 if [ ! -d $WALLPAPER_DIR ]; then
-  run "mkdir -p $WALLPAPER_DIR"
+  run mkdir -p $WALLPAPER_DIR
 fi
 
-run "wget -q -O ${WALLPAPER_FILE} ${IMAGE_URL}"
+run wget -q -O ${WALLPAPER_FILE} ${IMAGE_URL}
 if [ $? -eq 0 ]; then
   yak "T3X wallpaper download success"
 else
@@ -28,9 +28,9 @@ fi
 
 if got_command pcmanfm; then
   yak "pcmanfm command found - setting the image file as the wallpaper"
-  run "pcmanfm --set-wallpaper=${WALLPAPER_FILE}"
+  run pcmanfm --set-wallpaper=${WALLPAPER_FILE}
   yak "Refresh the desktop to show the new wallpaper"
-  run "pcmanfm -w /"
+  run pcmanfm -w /
   cowsay "Look at that" | lolcat
 else
   warn "pcmanfm command not found - skipping applying of wallpaper"
